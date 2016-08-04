193ad8b4873719cd53501b225c639a6120c75afa
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-08-04 10:04:49 -0700

    Tweaks

--- www/js/sabrina.js
+++ www/js/sabrina.js
@@ -53,7 +53,14 @@ $(function() {
 
     var sessionId = undefined;
     var utterance = undefined;
-    var counter = 0;
+
+    function counter() {
+        var v = localStorage.getItem('counter') || 0;
+        v++;
+        localStorage.setItem('counter', v);
+        return v;
+    }
+    $('#counter').text(localStorage.getItem('counter') || 0);
 
     function accept(event) {
         event.preventDefault();
@@ -65,16 +72,68 @@ $(function() {
         url += '&target=' + encodeURIComponent(a.attr('data-target-json'));
 
         $.getJSON(url, function(data) {
-            $('#results').empty();
+            $('#results-container').hide();
             if (data.error)
                 console.log('Error in learning', data.error);
             else
-                $('#counter').text(String(++counter));
+                $('#counter').text(String(counter()));
         });
+    }
+    // we can't train on a fully negative example, so we just do nothing
+    function rejectAll(event) {
+        event.preventDefault();
+        $('#results-container').hide();
+        $('#counter').text(String(counter()));
+    }
+
+
+    var DO_FALLBACK = {};
+    // this function implements the heuristics sabrina has on the client side
+    // to pick up a good prediction
+    function predict(candidates) {
+        var choice = null;
+        var fallbacks = [];
+
+        if (candidates.length === 0) {
+            console.log('Failed to analyze message (no candidate parses)');
+            return null;
+        } else {
+            var effectiveProb = 0;
+            var top = candidates[0];
+
+            for (var candidate of candidates) {
+                if (candidate.answer === top.answer)
+                    effectiveProb += candidate.prob;
+                else
+                    break;
+            }
+            if (effectiveProb > 0.9) {
+                choice = top;
+            } else if (effectiveProb > 0.5 && candidates[0].score >= 0) {
+                choice = top;
+            } else {
+                fallbacks = [];
+                for (var candidate of candidates) {
+                    if (fallbacks.length >= 5)
+                        break;
+                    if (candidate.prob < 0.15 || candidate.score < -10)
+                        break;
+                    fallbacks.push(candidate);
+                }
+
+            }
+        }
+
+        if (choice != null)
+            return choice;
+        if (fallbacks.length > 0)
+            return DO_FALLBACK;
+        else
+            return null;
     }
 
     var FILTERED_STUFF = new Set(["yes", "debug", "never mind", "hello", "thanks", "cool", "no", "sorry", "list", "here",
-        "at work", "at home", "false", "true"]);
+        "at work", "at home", "false", "true", "configure", "discover", "list devices", "list queries", "list commands"]);
 
     $('#form').submit(function(event) {
         event.preventDefault();
@@ -87,11 +146,27 @@ $(function() {
         url += '&q=' + encodeURIComponent(utterance);
         $.getJSON(url, function(data) {
             sessionId = data.sessionId;
+            $('#results-container').show();
             var results = $('#results');
             results.empty();
+
+            var prediction = predict(data.candidates);
+            console.log('prediction', prediction);
+            if (prediction === null) {
+                $('#prediction').text('Sabrina is confused. She searches for “' + utterance + '” on the web.');
+            } else if (prediction === DO_FALLBACK) {
+                $('#prediction').text('Sabrina is somewhat confused. She needs your help!');
+            } else {
+                $('#prediction').text(prediction.canonical);
+            }
+
+            var previous = null;
             data.candidates.forEach(function(result) {
                 if (FILTERED_STUFF.has(result.canonical))
                     return;
+                if (previous === result.answer)
+                    return;
+                previous = result.answer;
                 var link = $('<a href="#">')
                     .text(result.canonical.replace('`` ', '“').replace(' \'\'', '”'))
                     .addClass('result')
@@ -99,6 +174,17 @@ $(function() {
                     .click(accept);
                 results.append($('<li>').append(link));
             });
+            var link;
+            link = $('<a href="#">')
+                .text('Search for “' + utterance + '” on the web')
+                    .addClass('result')
+                    .click(rejectAll);
+            results.append($('<li>').append(link));
+            link = $('<a href="#">')
+                .text('None of the above')
+                    .addClass('result')
+                    .click(rejectAll);
+            results.append($('<li>').append(link));
         });
     });
 });
